c79c256c4cf9c8d28aeb3d709cec057cd36ab05c
Kim eunji <emliy112@naver.com>
2021-03-23 23:19:40 +0900

    패캠 스위프트 기초

--- 스위프트 기초/MyPlayground.playground/Contents.swift
+++ 스위프트 기초/MyPlayground.playground/Contents.swift
@@ -4,3 +4,22 @@ var str = "Hello, playground"
 
 let value = arc4random_uniform(100)
 print("\(value)")
+
+
+// 튜플
+
+let coordinates = (4,6)
+
+let x = coordinates.0
+let y = coordinates.1
+
+
+let coordinatesNamed = (x: 3, y: 8)
+
+let x2 = coordinatesNamed.x
+let y2 = coordinatesNamed.y
+
+let (x3, y3) = coordinatesNamed // 좌표값 한번에 가져오기
+x3
+y3
+
