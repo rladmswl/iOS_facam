daf0997a8e25af68f840dca5c016cc3145fe56e2
Kim eunji <emliy112@naver.com>
2021-03-24 00:19:27 +0900

    패캠 기초

--- SwiftBasic.playground/Contents.swift
+++ SwiftBasic.playground/Contents.swift
@@ -6,6 +6,7 @@ let value = arc4random_uniform(100)
 print("\(value)")
 
 
+
 // 튜플
 
 let coordinates = (4,6)
@@ -24,3 +25,38 @@ x3
 y3
 
 
+
+// boolean - 제어한다는 느낌
+
+let yes = true
+let no = false
+
+let isFourGreaterThanFive = 4 > 5
+
+if isFourGreaterThanFive {
+    print("참")
+} else {
+    print("거짓")
+}
+
+
+let a = 5
+let b = 10
+
+if a > b {
+    print("a가 크다 ")
+} else {
+    print("b가 크다")
+}
+
+
+let name1 = "Jin"
+let name2 = "Jason"
+
+let isTwoNameSame = name1 == name2
+
+if isTwoNameSame {
+    print("이름이 같다")
+} else {
+    print("이름이 다르다")
+}
