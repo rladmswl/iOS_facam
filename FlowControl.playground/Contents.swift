91e5d1506a8e45c6c808efc1a67337e6735bfdd8
Kim eunji <emliy112@naver.com>
2021-03-24 13:53:30 +0900

    패캠 기초

--- FlowControl.playground/Contents.swift
+++ FlowControl.playground/Contents.swift
@@ -2,8 +2,20 @@ import UIKit
 
 // while
 
+//var i = 0
+//while i < 10 {
+//    print(i)
+//    i += 1
+//}
+
+
 var i = 0
 while i < 10 {
     print(i)
+    
+    if i == 5 {
+        break
+    }
+    
     i += 1
 }
